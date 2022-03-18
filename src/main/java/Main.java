76d2a193420f88eaa8c41a12b5d5c94edb6015f6
chloe <chloe.buttigieg12@gmail.com>
2022-03-18 17:04:20 +0100

    tp is ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main (String[] args) {
+
         Reader readerMiniDic = new Reader("src/main/resources/miniDico.txt");
         Reader readerDic = new Reader("src/main/resources/dico.txt");
 
@@ -16,8 +17,92 @@ public class Main {
         displaySums(miniDicTable, dicTable);
         displayTableComposition(miniDicTable, dicTable);
 
+        //displayRenduTP2();
+
         //displayPossibleSums(miniDicTable, "src/main/possible-sums/possible-sums-for-mini-dic.txt", readerMiniDic.read());
         //displayPossibleSums(dicTable, "src/main/possible-sums/possible-sums-for-dic.txt", readerDic.read());
+    }
+
+    public static void displayRenduTP2() {
+        System.out.println("\n------------- RENDU TP2 -------------\n");
+
+        /* TEST : Stockage du mini-dictionnaire */
+
+        System.out.println("----- TEST : Stockage du mini-dictionnaire -----");
+        long start = System.currentTimeMillis();
+
+        Reader readerMiniDic = new Reader("src/main/resources/miniDico.txt");
+        HashTable tableMiniDic = new HashTable(readerMiniDic.read(), 3);
+
+        long end = System.currentTimeMillis();
+        double time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : 2-somme de 'adeeiilnnnorux' avec le mini-dictionnaire */
+
+        System.out.println("----- TEST : 2-somme de 'adeeiilnnnorux' avec le mini-dictionnaire -----");
+        start = System.currentTimeMillis();
+        System.out.println(getTwoSumDecomposition(tableMiniDic, "adeeiilnnnorux"));
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : 2-somme de 'aaaaeinnorsssstwz' avec le mini-dictionnaire */
+
+        System.out.println("----- TEST : 2-somme de 'aaaaeinnorsssstwz' avec le mini-dictionnaire -----");
+        start = System.currentTimeMillis();
+        System.out.println(getTwoSumDecomposition(tableMiniDic, "aaaaeinnorsssstwz"));
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : Stockage du dictionnaire */
+
+        System.out.println("----- TEST : Stockage du dictionnaire -----");
+        start = System.currentTimeMillis();
+
+        Reader readerDic = new Reader("src/main/resources/dico.txt");
+        HashTable tableDic = new HashTable(readerDic.read(), 3);
+
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : 2-somme de 'abeeiiillnnorrsstyzé' avec le dictionnaire */
+
+        System.out.println("----- TEST : 2-somme de 'abeeiiillnnorrsstyzé' avec le dictionnaire -----");
+        start = System.currentTimeMillis();
+        System.out.println(getTwoSumDecomposition(tableDic, "abeeiiillnnorrsstyzé"));
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : 2-somme de 'aaabdeiiillnorrrstzz' avec le dictionnaire */
+
+        System.out.println("----- TEST : 2-somme de 'aaabdeiiillnorrrstzz' avec le dictionnaire -----");
+        start = System.currentTimeMillis();
+        System.out.println(getTwoSumDecomposition(tableDic, "aaabdeiiillnorrrstzz"));
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : 2-somme de 'aadehimnnoorrttxz' avec le dictionnaire */
+
+        System.out.println("----- TEST : 2-somme de 'aadehimnnoorrttxz' avec le dictionnaire -----");
+        start = System.currentTimeMillis();
+        System.out.println(getTwoSumDecomposition(tableDic, "aadehimnnoorrttxz"));
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
+
+        /* TEST : 2-somme de 'buttigiegchloe' pour Buttigieg Chloe avec le dictionnaire */
+
+        System.out.println("----- TEST : 2-somme de 'buttigiegchloe' avec le dictionnaire -----");
+        start = System.currentTimeMillis();
+        System.out.println(getTwoSumDecomposition(tableDic, "buttigiegchloe"));
+        end = System.currentTimeMillis();
+        time = (end - start) / 1000.0;
+        System.out.println("Temps d'exécution : " + time + " seconde(s)\n");
     }
 
     public static void displaySums(final HashTable miniDicTable, final HashTable dicTable) {
